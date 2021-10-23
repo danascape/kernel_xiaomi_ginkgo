aosp-mirror/kernel_msm

overheat: replug to resume charging
Enable overheat mitigation monitoring based on USB online status, but
detect USB replug with USB present status. We re-enable CC line
detection for such a short period of time (hundreds of milliseconds)
that there isn't enough time for USB power supply to register as online.
The USB replug requirement was therefore broken when we switched from
using USB present status for monitoring to using USB online status.

Bug: 117527784
Bug: 120993204
Test: Kick off overheat mitigation on an in-box charger using fake_port_temp,
  then lower fake_port_temp below the hysteresis threshold and
  observe that mitigation doesn't clear until replug.
Change-Id: Iefc24c06b8d0e414a91db07c4d0e68aeda69e994
Signed-off-by: Vincent Wang <vincentwang@google.com>
Signed-off-by: Maggie White <maggiewhite@google.com>

drivers/power/supply/google/overheat_mitigation.c
@@ -46,6 +46,7 @@ struct overheat_info {
 	struct wakeup_source	   overheat_ws;
 
 	bool usb_connected;
+	bool usb_online;
 	bool accessory_connected;
 	bool usb_replug;
 	bool overheat_mitigation;
@@ -206,7 +207,22 @@ static int update_usb_status(struct overheat_info *ovh_info)
 	}
 
 	dev_dbg(ovh_info->dev, "Updating USB connected status\n");
+	/*
+	 * Update USB online status so that port_overheat_work can check it
+	 * before rescheduling.
+	 */
 	ret = GPSY_GET_PROP(ovh_info->usb_psy, POWER_SUPPLY_PROP_ONLINE);
+	if (ret < 0)
+		return ret;
+	ovh_info->usb_online = ret;
+
+	/*
+	 * Update USB present status to determine if USB has been disconnected.
+	 * If we use USB online status to determine replug, we will need to
+	 * extend the delay between re-enabling CC detection and checking the
+	 * USB online status.
+	 */
+	ret = GPSY_GET_PROP(ovh_info->usb_psy, POWER_SUPPLY_PROP_PRESENT);
 	if (ret < 0)
 		return ret;
 	ovh_info->usb_connected = ret;
@@ -226,7 +242,7 @@ static int update_usb_status(struct overheat_info *ovh_info)
 			   USB_OVERHEAT_MITIGATION_VOTER, true, 0);
 		if (ret < 0) {
 			dev_err(ovh_info->dev,
-				"Couldn't un-vote for disable_power_role_switch ret=%d\n",
+				"Couldn't vote for disable_power_role_switch ret=%d\n",
 				ret);
 			return ret;
 		}
@@ -334,10 +350,10 @@ static void port_overheat_work(struct work_struct *work)
 		goto rerun;
 	}
 
-	if (ovh_info->overheat_mitigation || ovh_info->usb_connected ||
+	if (ovh_info->overheat_mitigation || ovh_info->usb_online ||
 	    should_check_accessory(ovh_info))
 		goto rerun;
-	// Do not run again, USB port isn't overheated or connected to something
+	// Do not run again, USB port isn't overheated or registering as online
 	ovh_info->overheat_work_running = false;
 	__pm_relax(&ovh_info->overheat_ws);
 	return;
@@ -382,6 +398,7 @@ static int ovh_probe(struct platform_device *pdev)
 	ovh_info->usb_psy = usb_psy;
 	ovh_info->overheat_mitigation = false;
 	ovh_info->usb_replug = false;
+	ovh_info->usb_online = false;
 	ovh_info->usb_connected = false;
 	ovh_info->overheat_work_running = false;
 

danascape/kernel_xiaomi_ginkgo

Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

drivers/power/supply/google/overheat_mitigation.c
@@ -45,6 +45,7 @@ struct overheat_info {
 	struct wakeup_source	   overheat_ws;
 
 	bool usb_connected;
+	bool usb_online;
 	bool accessory_connected;
 	bool usb_replug;
 	bool overheat_mitigation;
@@ -227,7 +228,22 @@ static int update_usb_status(struct overheat_info *ovh_info)
 	}
 
 	dev_dbg(ovh_info->dev, "Updating USB connected status\n");
+	/*
+	 * Update USB online status so that port_overheat_work can check it
+	 * before rescheduling.
+	 */
 	ret = PSY_GET_PROP(ovh_info->usb_psy, POWER_SUPPLY_PROP_ONLINE);
+	if (ret < 0)
+		return ret;
+	ovh_info->usb_online = ret;
+
+	/*
+	 * Update USB present status to determine if USB has been disconnected.
+	 * If we use USB online status to determine replug, we will need to
+	 * extend the delay between re-enabling CC detection and checking the
+	 * USB online status.
+	 */
+	ret = PSY_GET_PROP(ovh_info->usb_psy, POWER_SUPPLY_PROP_PRESENT);
 	if (ret < 0)
 		return ret;
 	ovh_info->usb_connected = ret;
@@ -247,7 +263,7 @@ static int update_usb_status(struct overheat_info *ovh_info)
 			   USB_OVERHEAT_MITIGATION_VOTER, true, 0);
 		if (ret < 0) {
 			dev_err(ovh_info->dev,
-				"Couldn't un-vote for disable_power_role_switch ret=%d\n",
+				"Couldn't vote for disable_power_role_switch ret=%d\n",
 				ret);
 			return ret;
 		}
@@ -355,10 +371,10 @@ static void port_overheat_work(struct work_struct *work)
 		goto rerun;
 	}
 
-	if (ovh_info->overheat_mitigation || ovh_info->usb_connected ||
+	if (ovh_info->overheat_mitigation || ovh_info->usb_online ||
 	    should_check_accessory(ovh_info))
 		goto rerun;
-	// Do not run again, USB port isn't overheated or connected to something
+	// Do not run again, USB port isn't overheated or registering as online
 	ovh_info->overheat_work_running = false;
 	__pm_relax(&ovh_info->overheat_ws);
 	return;
@@ -403,6 +419,7 @@ static int ovh_probe(struct platform_device *pdev)
 	ovh_info->usb_psy = usb_psy;
 	ovh_info->overheat_mitigation = false;
 	ovh_info->usb_replug = false;
+	ovh_info->usb_online = false;
 	ovh_info->usb_connected = false;
 	ovh_info->overheat_work_running = false;
 